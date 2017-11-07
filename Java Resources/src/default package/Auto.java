86678ff2f97fcad53a21f0ee4ac0fd8966a43e78
iby1041 <32837623+iby1041@users.noreply.github.com>
2017-11-07 13:22:57 +0100

    Add files via upload

---
+++ Java Resources/src/default package/Auto.java
@@ -0,0 +1,63 @@
+
+public class Auto {
+
+	private String marke;
+	private String bezeichnung;
+	private String typ;
+	private String kraftstoff;
+	private String getriebe;
+	private int ps;
+	private int kw;
+	private int topspeed;
+	
+	public String getMarke() {
+		return marke;
+	}
+	public void setMarke(String marke) {
+		this.marke = marke;
+	}
+	public String getBezeichnung() {
+		return bezeichnung;
+	}
+	public void setBezeichnung(String bezeichnung) {
+		this.bezeichnung = bezeichnung;
+	}
+	public String getTyp() {
+		return typ;
+	}
+	public void setTyp(String typ) {
+		this.typ = typ;
+	}
+	public String getKraftstoff() {
+		return kraftstoff;
+	}
+	public void setKraftstoff(String kraftstoff) {
+		this.kraftstoff = kraftstoff;
+	}
+	public String getGetriebe() {
+		return getriebe;
+	}
+	public void setGetriebe(String getriebe) {
+		this.getriebe = getriebe;
+	}
+	public int getPs() {
+		return ps;
+	}
+	public void setPs(int ps) {
+		this.ps = ps;
+	}
+	public int getKw() {
+		return kw;
+	}
+	public void setKw(int kw) {
+		this.kw = kw;
+	}
+	public int getTopspeed() {
+		return topspeed;
+	}
+	public void setTopspeed(int topspeed) {
+		this.topspeed = topspeed;
+	}
+	
+	
+}
