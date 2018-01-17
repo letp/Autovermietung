fe3d311a135257762d03aa76c9999424e9d78fdb
letp <pel1217@thi.de>
2018-01-17 23:17:35 +0100

    Update UpdateServlet.java

--- Java Resources/src/default package/UpdateServlet.java
+++ Java Resources/src/default package/UpdateServlet.java
@@ -3,8 +3,6 @@
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-
 import javax.annotation.Resource;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
