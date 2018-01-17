0bcfa87d1a5a3fe40ed7b2df3860864b6fc7242c
letp <pel1217@thi.de>
2018-01-17 23:15:10 +0100

    Update LoginServlet.java

--- Java Resources/src/default package/LoginServlet.java
+++ Java Resources/src/default package/LoginServlet.java
@@ -1,115 +1,114 @@
-<!-- Peter -->
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-	pageEncoding="UTF-8"%>
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html>
-<head>
-<base href="${pageContext.request.requestURI}" />
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>Startseite</title>
-<link rel="stylesheet" type="text/css" href="../css/slider.css">
+/** Peter */
 
-</head>
-<body>
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 
-	<%@ include file="Navigation.jsp"%>
+import javax.annotation.Resource;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.sql.DataSource;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+	@Resource(lookup="jdbc/MyTHIPool")
+    DataSource ds;   
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Eingaben von Login Feldern abholen
+	    
+	    HttpSession session = request.getSession();
+	    Account form = new Account();
+        
+        form.setEmail(request.getParameter("email"));
+        form.setPasswort(request.getParameter("passwort"));
+        
+	       ResultSet rs = null;
+	       try(Connection con = ds.getConnection();
+	               
+	               PreparedStatement p = con.prepareStatement("SELECT * FROM accounts WHERE email = ? AND passwort = ?")){
+	               
+	               p.setString(1, request.getParameter("email"));
+	               p.setString(2, request.getParameter("passwort"));
+	               
+	               rs = p.executeQuery();
+	               if(rs.next()){
+	                   form.setName(rs.getString("name"));
+	                   form.setVorname(rs.getString("vorname"));
+	                   form.setStrasse(rs.getString("strasse"));
+	                   form.setHnr(rs.getString("hausnummer"));
+	                   form.setStadt(rs.getString("stadt"));
+	                   form.setPlz(rs.getString("plz"));
+	                   form.setId(rs.getInt("id"));
+	                   session.setAttribute("account", form);
+	                   session.setAttribute("logged", 1);
+	                   ArrayList<Auto> autos = findAutos(form.getId());
+	                   session.setAttribute("autos", autos);
+	                   final RequestDispatcher dispatcher = request.getRequestDispatcher("home/html/Konto.jsp");
+	                   dispatcher.forward(request, response);
+	               }else{
+	                   session.setAttribute("logfalsch", 1);
+	                   final RequestDispatcher dispatcher = request.getRequestDispatcher("home/html/Login.jsp");
+                       dispatcher.forward(request, response);
+	               }
+	               
+	               
+	        } catch (Exception ex) {
+	             // TODO Auto-generated catch block
+	             throw new ServletException(ex.getMessage());
+	        }
+	}
 	
-	<header>
-	<h1 role="heading" align="center">Autovermietung</h1>
-	</header>
+	public ArrayList<Auto> findAutos(int id) throws ServletException{
+        ResultSet rs = null;
+        ArrayList<Auto> autos = new ArrayList<Auto>();
+        try(Connection con = ds.getConnection();
+                PreparedStatement p = con.prepareStatement("SELECT * FROM autos INNER JOIN accounts On autos.vermieterID = accounts.id WHERE accounts.id = ?")){
+                p.setInt(1, id);
+                rs = p.executeQuery();
+                while(rs.next()){
+                        Auto auto = new Auto();
+                        auto.setModell(rs.getString("modell"));
+                        auto.setId(rs.getInt("id"));
+                        auto.setMarke(rs.getString("marke"));
+                        autos.add(auto);
+                }
+         } catch (Exception ex) {
+              // TODO Auto-generated catch block
+              throw new ServletException(ex.getMessage());
+         }
+        return autos;
+	}
+	
+	 
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
 
-	<!--  vgl. https://www.w3schools.com/howto/howto_js_slideshow.asp -->
-	<div class=slideshow-news>
-
-		<div class="slider fade" align="center">
-			<div class="bildnummer">1 / 2</div>
-			<img src="../image/Auto.jpg" style="width: 800px; height: 400px">
-			<div class="text">Welcome</div>
-		</div>
-		<div class="slider fade" align="center">
-			<div class="bildnummer">2 / 2</div>
-			<img src="../image/vermietung.jpg" style="width: 1200px; max-height: 400px">
-			<div class="text">Welcome</div>
-		</div>
-
-
-		<button class="prev" onclick="plusSlides(-1)">&#10094;</button>
-		<button class="next" onclick="plusSlides(1)">&#10095;</button>
-
-
-	</div>
-	<div style="text-align: center">
-		<span class="dot" onclick="currentSlide(1)"></span> <span class="dot"
-			onclick="currentSlide(2)"></span> <span class="dot"
-			onclick="currentSlide(3)"></span>
-	</div>
-
-	<h2 >Aktuelle Autos</h2>
-	<hr />
-	<div id="neu"></div>
-
-	<br>
-
-	<footer class="foot"> <br>
-
-	Autovermietung <br>
-	<a href="Login.jsp">Login</a> <br>
-	<a href="NewAccount.jsp">Registrieren</a> <br>
-	</footer>
-
-	<script>
-		document.addEventListener("DOMContentLoaded", getneu);
-
-		var slideIndex = 1;
-		showSlides(slideIndex);
-
-		function plusSlides(n) {
-			showSlides(slideIndex += n);
-		}
-
-		function currentSlide(n) {
-			showSlides(slideIndex = n);
-		}
-
-		function showSlides(n) {
-			var i;
-			var slides = document.getElementsByClassName("slider");
-			var dots = document.getElementsByClassName("dot");
-			if (n > slides.length) {
-				slideIndex = 1
-			}
-			if (n < 1) {
-				slideIndex = slides.length
-			}
-			for (i = 0; i < slides.length; i++) {
-				slides[i].style.display = "none";
-			}
-			slides[slideIndex - 1].style.display = "block";
-		}
-
-		function getneu() {
-			var xmlhttp = new XMLHttpRequest();
-			xmlhttp.onreadystatechange = function() {
-				console.log("Callback reached with status " + xmlhttp.status
-						+ " and ready State " + xmlhttp.readyState);
-				if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-					var neu = JSON.parse(xmlhttp.responseText);
-					var ausgabe = "<figure>";
-					for (var i = 0; i < neu.length; i++) {
-
-						ausgabe += "<figure>";
-						ausgabe += "<a>" + neu[i].modell"</a>";
-						ausgabe += "</figure>";
-
-					}
-					ausgabe += "</figure>";
-
-					document.getElementById("neu").innerHTML = ausgabe;
-				}
-			}
-			xmlhttp.open("GET", "../../NeuServlet", true);
-			xmlhttp.send();
-		}
-	</script>
-</body>
-</html>
+}
